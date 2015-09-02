020f611550e31dd3346ccb5fd0e5382b5e7cd16e
David Pawlowski <dpawlows@emich.edu>
2015-09-02 10:47:04 -0400

    updated tempsettings again

--- physicsgenome/tempsettings.py
+++ physicsgenome/tempsettings.py
@@ -107,4 +107,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+
